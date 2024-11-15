852643373c6f1d90bb422decc32bad116dc11632
Алексей Коврижных <alexkovrizh@gmail.com>
2024-11-15 17:09:04 +0300

    Дада

--- WorkCurs/WorkCurs.cpp
+++ WorkCurs/WorkCurs.cpp
@@ -5,88 +5,69 @@
 using namespace std;
 
 
-int** matrixfill(int& row, int& col, string num) {
-    string textmatrix;
+int** matrixfill(int& size, string num) {
+    char textmatrix[1000];
     ifstream nums("matrix.txt");
-    if (nums.is_open())
-    {
-        getline(nums, textmatrix);
-    }
-    else {
+    if (!nums.is_open()) {
         cout << "error";
-        return false;
+        return nullptr;
     }
 
-    cout << "\tПри чтение матрицы из файла matrix.txt, должны соблюдаться условия.\n 1.Три матрицы должны быть разделины восклицательным знаком <!>.\n 2.В конце каждой строки дожна быть точка с запятой." << endl;
-    int** matrix = new int* [row];
-    for (int i = 0; i < row; i++) {
-        matrix[i] = new int[col];
+    int count = 0;
+    while (count < 1000 && nums >> textmatrix[count]) {
+        count++;
     }
-    short num[1000];
-    short count = 0, col = 1, row = 1;
-    for (short i = 0; i < 1000; i++)
-    {
-        if (textmatrix[i] == " ")
-        {
-            col++;
-            continue;
-        }
-        if (textmatrix[i] == ";")
-        {
-            row++;
-            continue;
-        }
-        if (textmatrix[i] == "!")
-        {
-            break;
-        }
-        num[count++] = stoi(textmatrix);
+
+    cout << "Введите размерность " << num << "матрицы" << endl;
+    cin >> size;
+
+    int** matrix = new int*[size];
+    for (int i = 0; i < size; i++) {
+        matrix[i] = new int[size];
     }
-    short a = 0;
-    for (short i = 0; i < row; i++)
-    {
-        for (short j = 0; j < col; j++)
-        {
-            matrix[i][j] = num[a++];
-        }
+
+    for (int i = 0; i < size * size && i < count; i++) {
+        matrix[i / size][i % size] = stoi(&textmatrix[i]);
     }
+
     return matrix;
 }
 
-int** matrixcons(int& row, int& col, string num) {
-    cout << "Задайте размерность " << num << " матрицы: ";
-    cin >> row >> col;
 
-    int** matrix = new int* [row]; 
-    for (int i = 0; i < row; i++) {
-        matrix[i] = new int[col];
+int** matrixcons(int& size, string num) {
+    cout << "Задайте размерность " << num << " матрицы: ";
+    cin >> size;
+
+    int** matrix = new int* [size];
+    for (int i = 0; i < size; i++) {
+        matrix[i] = new int[size];
     }
 
     system("cls");
     cout << "Заполните матрицу" << endl;
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
             cin >> matrix[i][j];
         }
     }
     system("cls");
 
-    return matrix; 
+    return matrix;
 }
 
-void clear_memory(int** matrix, int row) {
-    for (int i = 0; i < row; i++) {
+void clear_memory(int** matrix, int size) {
+    for (int i = 0; i < size; i++) {
         delete[] matrix[i];
     }
     delete[] matrix;
 }
 
 int main() {
-    setlocale(LC_ALL, "RU");
+    setlocale(LC_ALL, "Russian");
     int** matrixfirst = nullptr;
     int** matrixsecond = nullptr;
     int** matrixthrid = nullptr;
-    int row1, col1, row2, col2, row3, col3;
+    int size1, size2, size3;
 
     while (true) {
         cout << "\tВыберите действие." << endl;
@@ -99,23 +80,23 @@ int main() {
         switch (choice) {
         case 1:
             system("cls");
-            matrixfirst = matrixcons(row1, col1, "первой");
-            matrixsecond = matrixcons(row2, col2, "второй");
-            matrixthrid = matrixcons(row3, col3, "третьей");
+            matrixfirst = matrixcons(size1, "первой");
+            matrixsecond = matrixcons(size2, "второй");
+            matrixthrid = matrixcons(size3, "третьей");
             break;
         case 2: 
-            matrixfirst = matrixfill(row1, col1, "первой");
-            matrixsecond = matrixfill(row2, col2, "второй");
-            matrixthrid = matrixfill(row3, col3, "третьей");
+            matrixfirst = matrixfill(size1,"первой");
+            matrixsecond = matrixfill(size1, "второй");
+            matrixthrid = matrixfill(size3, "третьей");
             
             
             break;
             //case 3: editmatrix(); break;
             //case 4: viewmatrix(); break;
         case 5:
-            clear_memory(matrixfirst, row1);
-            clear_memory(matrixsecond, row2);
-            clear_memory(matrixthrid, row3);
+            clear_memory(matrixfirst, size1);
+            clear_memory(matrixsecond, size2);
+            clear_memory(matrixthrid, size3);
             return 0;
         default:
             cout << "error\n";
